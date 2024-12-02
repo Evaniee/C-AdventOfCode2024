7a6a295122f71c176b1c4e56be94ad5be05b115b
Evaniee <evaniee7@outlook.com>
2024-12-02 12:46:45 +0000

    Added Day 1 Part 2

--- C#AdventOfCode2024/Day1.cs
+++ C#AdventOfCode2024/Day1.cs
@@ -27,5 +27,32 @@ namespace C_AdventOfCode2024
             int totalDistance = Math.Abs(distance.Sum());
             Console.WriteLine($"Total Distance: {totalDistance}");
         }
+    
+        public static void Part2()
+        {
+            string[] lines = File.ReadAllLines("Resources\\Day1\\input.txt");
+
+            List<int> locations1 = new List<int>();
+            List<int> locations2 = new List<int>();
+            foreach (string line in lines)
+            {
+                string[] splits = line.Split("   ");
+                locations1.Add(int.Parse(splits[0]));
+                locations2.Add(int.Parse(splits[1]));
+            }
+
+            locations1.Sort();
+            locations2.Sort();
+
+            Dictionary<int, int> similarityScores = new Dictionary<int, int>();
+            for (int i = 0; i < locations1.Count; i++)
+                similarityScores[locations1[i]] = locations2.Where(x => x == locations1[i]).Count();
+
+            int sum = 0;
+            foreach(KeyValuePair<int, int> score in similarityScores)
+                sum += score.Key * score.Value;
+
+            Console.WriteLine($"Similarity Sum: {sum}");
+        }
     }
 }
--- C#AdventOfCode2024/Program.cs
+++ C#AdventOfCode2024/Program.cs
@@ -8,11 +8,19 @@
             Console.Write("Select a day:");
             while (!int.TryParse(Console.ReadLine(), out day) || day < 1 || day > 31)
                 Console.Write("Error: Invalid day, retry.\nSelect a day: ");
-            
-            switch(day)
+
+            int choice;
+            Console.Write("Select a choice:");
+            while (!int.TryParse(Console.ReadLine(), out choice) || (choice != 1 && choice != 2))
+                Console.Write("Error: Invalid day, retry.\nSelect a day: ");
+
+            switch (day)
             {
                 case 1:
-                    Day1.Part1();
+                    if(choice == 1)
+                        Day1.Part1();
+                    else if (choice == 2)
+                        Day1.Part2();
                     break;
                 default:
                     Console.WriteLine("Unimplemented");
