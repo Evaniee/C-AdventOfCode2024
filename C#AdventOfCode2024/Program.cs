c8397e5d832964b7af3edf6617cef94d12d8699d
Evaniee <evaniee7@outlook.com>
2024-12-02 11:27:36 +0000

    Add project files.

---
+++ C#AdventOfCode2024/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace C_AdventOfCode2024
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int day;
+            Console.Write("Select a day:");
+            while (int.TryParse(Console.ReadLine(), out day) || day < 1 || day > 31)
+                Console.Write("Error: Invalid day, retry.\nSelect a day: ");
+            
+            switch(day)
+            {
+                case 1:
+                    break;
+                default:
+                    Console.WriteLine("Unimplemented");
+                    break;
+            }
+        }
+    }
+}
